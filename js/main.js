85da08974abf62a6b12a2a0895beb40d53824424
Abelyem <abel.yemane1@hotmail.co.uk>
2020-07-06 11:58:55 +0000

    Added container with div tags inside (for cocktail info/images to be entered later) / added API link to random cocktail generator / named tags to be used later (within main.js)- not yet created in HTML / named function to be used (yet to add to index.html)

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+
+function ************************************************************ randomDrink() {  
+fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+.then (response => {
+
+    return response.json()
+})
+
+.then(data => {
+
+   const name = data.drinks[0].strDrink;
+   const category = data.drinks[0].strCategory;
+   const instructions = data.drinks[0].strInstructions;
+  // const category = data.drinks[0].strCategory;
+
+   drinkName.innerHTML = name;
+   typeOfDrink.innerHTML = category;
+   howToMake.innerHTML = instructions;
+
+})} 
