8c1d1bbfabc6a74b10884e85cf7db6fb9067ede7
Abelyem <abel.yemane1@hotmail.co.uk>
2020-07-08 11:07:05 +0000

    Added class/id name to divs which will contain API data (name/image/instructions on how to make the cocktail)/ finalised onclick eventhandler which will produce this information / added hover via css on divs which will contain the cocktail API information (have commented out javascript function onmouseover and onmouseout as this was achieved via css hover)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+const drinkName = document.getElementById('drink-name')
+const typeOfDrink = document.getElementById('category')
+const howToMake = document.getElementById('drink-instructions')
+const randomDrinkButton = document.getElementById('drink-button')
 
 function randomDrink() {  
 fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
@@ -9,23 +13,32 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
 .then(data => {
 
    const name = data.drinks[0].strDrink;
+   const image = data.drinks[0].strDrinkThumb;
    const category = data.drinks[0].strCategory;
    const instructions = data.drinks[0].strInstructions;
-  // const category = data.drinks[0].strCategory;
 
-   drinkName.innerHTML = name;
+   drinkName.innerHTML = name + `<img src="${image}/preview"></img>`;
    typeOfDrink.innerHTML = category;
    howToMake.innerHTML = instructions;
 
 })} 
 
-XXXXXX.addEventListener('click', randomDrink)
+randomDrinkButton.addEventListener('click', randomDrink)
 
 
-function colorChangeToRed() {
-  document.getElementById("topCocktails").style.backgroundColor = "red";
-}
+// function colorChangeToRed() {
+// var cocktailsList = document.getElementsByClassName("topCocktails")
+// var i;
+// for (i = 0; i < cocktailsList.length; i++) {
+// cocktailsList[i].style.backgroundColor = "red";
+// }}
 
-function ColorRevertToOriginal() {
-  document.getElementById("topCocktails").style.backgroundColor = "aqua";
-}
+// function ColorRevertToOriginal() {
+// var cocktailsList = document.getElementsByClassName("topCocktails")
+// var i;
+// for (i = 0; i < cocktailsList.length; i++) {
+// cocktailsList[i].style.backgroundColor = "aqua";
+// }}
+
+
+
