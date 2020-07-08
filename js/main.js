2b59eedc387df3ba203935de5f5f112e90b875ab
Abelyem <abel.yemane1@hotmail.co.uk>
2020-07-08 21:07:47 +0000

    Removed the commented out array (ingredient1 - no longer required as there is the updated ingredient1 array which is being used) / added bootstrap CDN link to html)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const drinkName = document.getElementById('drink-name')
-const typeOfDrink = document.getElementById('category')
+const ingredientsRequired = document.getElementById('ingredients')
 const howToMake = document.getElementById('drink-instructions')
 const randomDrinkButton = document.getElementById('drink-button')
 
@@ -17,9 +17,28 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
    const category = data.drinks[0].strCategory;
    const instructions = data.drinks[0].strInstructions;
 
-   drinkName.innerHTML = name + `<img src="${image}/preview"></img>`;
-   typeOfDrink.innerHTML = category;
-   howToMake.innerHTML = instructions;
+   drinkName.innerHTML = `<h3>${name}</h3><img src="${image}/preview"></img>`;
+   howToMake.innerHTML = `<h4>${category}</h4> ${instructions}`;
+  
+const ingredient1 = [data.drinks[0].strIngredient1, data.drinks[0].strIngredient2, data.drinks[0].strIngredient3,
+                    data.drinks[0].strIngredient4, data.drinks[0].strIngredient5, data.drinks[0].strIngredient6, 
+                    data.drinks[0].strIngredient7, data.drinks[0].strIngredient8, data.drinks[0].strIngredient9,
+                    data.drinks[0].strIngredient10, data.drinks[0].strIngredient11, data.drinks[0].strIngredient12,
+                    data.drinks[0].strIngredient13, data.drinks[0].strIngredient14, data.drinks[0].strIngredient15]
+
+let i;
+
+for (i = 0; i < ingredient1.length; i++) {
+ if (ingredientsRequired.innerHTML == null) {
+     return ingredientsRequired.innerHTML = "";
+ }  else {
+    ingredientsRequired.innerHTML = ingredient1.join(`<br>`);
+ }
+}
+ 
+
+
+
 
 })} 
 
