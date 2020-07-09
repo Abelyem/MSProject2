5f47d922aa74d661430859b54a5fd49bcec46bb8
Abelyem <abel.yemane1@hotmail.co.uk>
2020-07-09 09:58:15 +0000

    Added loader gif before random cocktail is generated / added a new array for the measurements required (to go along with the ingredients required for the random cocktail generated)/ added bested grid to display the ingredients and measurements in one div (side by side so it is clear how much of what is required)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const drinkName = document.getElementById('drink-name')
 const ingredientsRequired = document.getElementById('ingredients')
 const howToMake = document.getElementById('drink-instructions')
+const measurementsRequired = document.getElementById('measurements')
 const randomDrinkButton = document.getElementById('drink-button')
 
 function randomDrink() {  
@@ -17,28 +18,40 @@ fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
    const category = data.drinks[0].strCategory;
    const instructions = data.drinks[0].strInstructions;
 
-   drinkName.innerHTML = `<h3>${name}</h3><img src="${image}/preview"></img>`;
+   drinkName.innerHTML = `<h4>${name}</h4><img src="${image}/preview"></img>`;
    howToMake.innerHTML = `<h4>${category}</h4> ${instructions}`;
   
-const ingredient1 = [data.drinks[0].strIngredient1, data.drinks[0].strIngredient2, data.drinks[0].strIngredient3,
+const AllIngredients = [data.drinks[0].strIngredient1, data.drinks[0].strIngredient2, data.drinks[0].strIngredient3,
                     data.drinks[0].strIngredient4, data.drinks[0].strIngredient5, data.drinks[0].strIngredient6, 
                     data.drinks[0].strIngredient7, data.drinks[0].strIngredient8, data.drinks[0].strIngredient9,
                     data.drinks[0].strIngredient10, data.drinks[0].strIngredient11, data.drinks[0].strIngredient12,
                     data.drinks[0].strIngredient13, data.drinks[0].strIngredient14, data.drinks[0].strIngredient15]
 
-let i;
+const AllMeasurements = [data.drinks[0].strMeasure1, data.drinks[0].strMeasure2, data.drinks[0].strMeasure3,
+                        data.drinks[0].strMeasure4, data.drinks[0].strMeasure5, data.drinks[0].strMeasure6,
+                        data.drinks[0].strMeasure7, data.drinks[0].strMeasure8, data.drinks[0].strMeasure9,
+                        data.drinks[0].strMeasure10, data.drinks[0].strMeasure11, data.drinks[0].strMeasure12,
+                        data.drinks[0].strMeasure13, data.drinks[0].strMeasure14, data.drinks[0].strMeasure15]
 
-for (i = 0; i < ingredient1.length; i++) {
+let i;
+let j;
+
+for (i = 0; i < AllIngredients.length; i++) {
  if (ingredientsRequired.innerHTML == null) {
      return ingredientsRequired.innerHTML = "";
  }  else {
-    ingredientsRequired.innerHTML = ingredient1.join(`<br>`);
+    ingredientsRequired.innerHTML = AllIngredients.join(`<br>`);
+ }
+}
+
+for (j = 0; j < AllMeasurements.length; j++) {
+ if (measurementsRequired.innerHTML == null) {
+     return measurementsRequired.innerHTML = "";
+ }  else {
+    measurementsRequired.innerHTML = AllMeasurements.join(`<br>`);
  }
 }
  
-
-
-
 
 })} 
 
